34be505e94881033020348078eefae07f7dd7369
Borchsenius <andreas@kobaj.com>
2024-02-16 22:21:55 +0100

    Adding new heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero = 'Windstorm';
+
+}
