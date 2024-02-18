e494f6b292460148eafed244e01da7546827be62
Borchsenius <andreas@kobaj.com>
2024-02-18 12:33:13 +0100

    Part II - Mock services and display list

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+
+}
