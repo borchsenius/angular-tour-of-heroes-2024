4f77b271054ac8a29d2ded424696d2c3818219c3
Borchsenius <andreas@kobaj.com>
2024-02-18 11:51:44 +0100

    Heroes pt 1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm II of Interface'
+  };
 }
