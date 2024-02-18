87fc6c807f367ebef2524c13636529d6b418b398
Borchsenius <andreas@kobaj.com>
2024-02-18 12:32:06 +0100

    Part II - Mock hero services and displaying a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
+import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm II of Interface'
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log("click: " + hero.name);
+  }
+
 }
