fa1419c7115606e8700c1ac5c3d6126a8e4fba52
Borchsenius <andreas@kobaj.com>
2024-02-25 09:58:08 +0100

    retrieving data from a in mem DB

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,25 +3,36 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+  constructor(  
+      private http: HttpClient,
+      private messageService: MessageService) { }
   /*
   getHeroes(): Hero[] {
     return HEROES;
   }
   */
 
+  /*
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+  */
 
+  getHeroes(): Observable<Hero[]> {
+    //const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetching heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+  
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
@@ -29,5 +40,5 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
-  
+
 }
