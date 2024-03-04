d8ca50edcfb5ea9a1f36896bcf7e9fd10a2ad326
Borchsenius <andreas.westh@gmail.com>
2024-03-04 23:46:23 +0100

    applying animation fadeIn and slidein

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { LogoComponent } from './logo/logo.component';
-
+import {FadeIn} from './animation';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [HeroesComponent, MessagesComponent, LogoComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    FadeIn(800, true)
+    
+  ]
 })
 export class AppComponent {
   title = 'Tour of Heroes 2024';
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,13 +4,17 @@ import { HeroService } from '../hero.service';
 import { RouterLink } from '@angular/router';
 import { NgForOf } from '@angular/common';
 import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { FadeIn } from '../animation';
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
   imports: [RouterLink, NgForOf, HeroSearchComponent],
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: [ './dashboard.component.css' ],
+  animations: [
+    FadeIn(800, false),
+  ]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
